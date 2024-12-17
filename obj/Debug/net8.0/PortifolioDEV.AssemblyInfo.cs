7cfbabee1352f4948d644884fce6c03a5dc65b71
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-12-17 15:18:18 -0300

    aula1712

--- obj/Debug/net8.0/PortifolioDEV.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortifolioDEV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortifolioDEV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe02bc979ccbcfeb089b731184d2687ebb05626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e94bc881b93f037d4f3b27c4fe8052e4721100")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortifolioDEV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortifolioDEV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
